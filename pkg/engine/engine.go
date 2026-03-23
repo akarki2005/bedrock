daa4257e15cf9ba5ab1707c435909e9eeb7da821
Aayush Karki <akarki2005@gmail.com>
2026-03-23 05:36:58 -0400

    built engine api

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -1,1 +1,65 @@
-// TODO: public API for the storage engine
+package engine
+
+import (
+	"fmt"
+
+	"github.com/akarki2005/lsm-engine/internal/entry"
+	"github.com/akarki2005/lsm-engine/internal/memtable"
+	"github.com/akarki2005/lsm-engine/internal/wal"
+)
+
+type Engine struct {
+	wal      *wal.WAL
+	memtable *memtable.MemTable
+}
+
+func Open(path string) (*Engine, error) {
+	wal, err := wal.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("open WAL: %w", err)
+	}
+
+	mt := memtable.New()
+
+	if err := wal.Replay(mt.Put); err != nil {
+		_ = wal.Close()
+		return nil, fmt.Errorf("replay WAL into memtable: %w", err)
+	}
+
+	return &Engine{
+		wal:      wal,
+		memtable: mt,
+	}, nil
+}
+
+func (e *Engine) Put(key, value []byte) error {
+	ent := entry.New(key, value)
+
+	if err := e.wal.Append(ent); err != nil {
+		return fmt.Errorf("append to WAL: %w", err)
+	}
+
+	if err := e.memtable.Put(ent); err != nil {
+		return fmt.Errorf("put into memtable: %w", err)
+	}
+
+	return nil
+}
+
+func (e *Engine) Get(key []byte) ([]byte, bool, error) {
+	ent, ok := e.memtable.Get(key)
+
+	if !ok {
+		return nil, false, nil
+	}
+
+	value := append([]byte(nil), ent.Value...)
+	return value, true, nil
+}
+
+func (e *Engine) Close() error {
+	if err := e.wal.Close(); err != nil {
+		return fmt.Errorf("close WAL: %w", err)
+	}
+	return nil
+}
